0e0e40cedefa6eb861eb3b10ef6c324c64401a3f
aak201610885 <aak201610885.com>
2021-08-21 19:41:26 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header>
+				{" "}
+				<a href="#">
+					<h2>store</h2>
+				</a>
+			</header>
+			<main>
+				<h1>main</h1>
+			</main>
+			<footer>
+				<h1>footer</h1>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
